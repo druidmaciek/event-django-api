bccd74f2dd715a2f81ce621b41abed5cbff04986
Maciek <maciej@janowski.dev>
2022-11-23 13:28:40 -0300

    add swagger

--- app/events_api/urls.py
+++ app/events_api/urls.py
@@ -15,13 +15,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_yasg import openapi
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
 
 from events.views import EventList, EventDetail, ExportCSVEvents
 from performances.views import PerformanceList, PerformanceDetail
-
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Movies API",
+        default_version="v1",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "swagger-docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
     path("api/events/export/", ExportCSVEvents.as_view()),
     path("api/events/", EventList.as_view()),
     path("api/events/<pk>/", EventDetail.as_view()),
